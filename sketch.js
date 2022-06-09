b54954fa63e0db39ad9a8621f96c7a3dc6c956af
marlonbarrios <marlon@dance-tech.net>
2022-06-08 23:09:38 -0400

    no capture

--- sketch.js
+++ sketch.js
@@ -1,3 +1,7 @@
+//Soft-space #2: Slitscan imaging techniques are used to create static images of time-based phenomena. In traditional film photography, slit scan images are created by exposing film as it slides past a slit-shaped aperture. In the digital realm, thin slices are extracted from a sequence of video frames, and concatenated into a new image.
+
+//concept and programminf: Marlon Barrios Solano
+
 var video;
 var x = 0;
 
@@ -7,6 +11,7 @@ function setup() {
   video = createCapture(VIDEO)
   video.size(windowWidth,windowHeight);
   background(51);
+  video.hide();
 }
 
 function draw() {
