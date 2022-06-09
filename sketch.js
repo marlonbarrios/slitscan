7ad264a5ea3b31d9afd0a36e2a1d8f01d484af98
marlonbarrios <marlon@dance-tech.net>
2022-06-09 15:01:02 -0400

    correction of slit lin

--- sketch.js
+++ sketch.js
@@ -4,31 +4,49 @@
 
 var video;
 var x = 0;
+var y = 0;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   pixelDensity(1)
   video = createCapture(VIDEO)
   video.size(windowWidth,windowHeight);
+
   background(51);
+
   video.hide();
 }
 
 function draw() {
 
   video.loadPixels();
+
+
 //image(video,0,0)
 var w = video.width;
 var h = video.height;
+stroke('white')
 
-copy(video, w/2, 0, 1, h, x , 0, 1, h)
+
+// copy(video, 0, y, w , 1,  0, y, w , 1,)
+// y = y +1 ;
+// if (y > h) {
+// y = 0;
+// }
+copy(video,w/2, 0, 1 , h, x , 0, 1, h)
+
 x = x +1 ;
 if (x > w) {
 x = 0;
 }
 
+
+
+
+
+
 }
 
 function mousePressed(){
-  saveFrames('slitscan', 'png', 1, 1);
+  saveFrames('horizomtal-slitscan', 'png', 1, 1);
 }
